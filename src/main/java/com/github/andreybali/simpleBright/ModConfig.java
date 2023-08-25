26877ceb97ff6a4b9ee6c68fa8e4c2e4030f1c65
AndreyBali <@>
2023-08-25 12:27:43 +0700

    simpleBright

---
+++ src/main/java/com/github/andreybali/simpleBright/ModConfig.java
@@ -0,0 +1,20 @@
+package com.github.andreybali.simpleBright;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+
+@Config(name = "simpleBright")
+public class ModConfig implements ConfigData {
+    @ConfigEntry.Gui.Excluded
+    public static ModConfig INSTANCE;
+
+    public static void init() {
+        AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
+        INSTANCE = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+    }
+    @ConfigEntry.BoundedDiscrete(min = 1, max = 3)
+    public float brightLevel = 1.5f;
+}
